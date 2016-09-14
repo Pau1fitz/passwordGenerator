f1281cc72ee7e866ef220f93fc5f1ddd70a107b1
Fitzgerald <fitz035@gbmc02m62e6f8j4.bmg.bagint.com>
2016-09-14 09:58:58 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,19 @@
+Password-gen
+
+## Installation
+
+```
+  npm install password-gen --save
+```
+
+## Usage
+
+```
+  var passwordGen = require('password-gen');
+
+  passwordGen.generatePassword(passwordlength);
+```
+
+## Release History
+
+* 0.1.0 Initial release
---
+++ index.js
@@ -0,0 +1,13 @@
+const combination = 'abcdefghijklmnopqrstuvwxyz123456789';
+const password = '';
+
+module.exports.generatePassword = function(length) {
+	if (!length) {
+		length = 8;
+	}
+	var password = '';
+	for(var i = 0; i < length; i++) {
+		password += combination.charAt(Math.floor(Math.random() * combination.length));
+	}
+	return password;
+}
